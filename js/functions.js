e5e36fb981b546d137e6b5dbbd4b0def7f781e48
TauMare <sasas.sasas.972@gmail.com>
2023-06-22 16:03:38 +0700

    Переделанное задание с учетом верстки.

--- js/functions.js
+++ js/functions.js
@@ -17,13 +17,3 @@ function checkPalindrome(inputString){
     }
     return true
 }
-
-document.getElementsByClassName('check-length-result')[0].innerHTML = checkLength('проверяемая строка', 20);
-document.getElementsByClassName('check-length-result')[1].innerHTML = checkLength('проверяемая строка', 18);
-document.getElementsByClassName('check-length-result')[2].innerHTML = checkLength('проверяемая строка', 10);
-
-document.getElementsByClassName('check-palindrome-result')[0].innerHTML = checkPalindrome('топот');
-document.getElementsByClassName('check-palindrome-result')[1].innerHTML = checkPalindrome('ДовОд');
-document.getElementsByClassName('check-palindrome-result')[2].innerHTML = checkPalindrome('Кекс');
-document.getElementsByClassName('check-palindrome-result')[3].innerHTML = checkPalindrome('Лёша на полке клопа нашёл ');
-document.getElementsByClassName('check-palindrome-result')[4].innerHTML = checkPalindrome(2023);
