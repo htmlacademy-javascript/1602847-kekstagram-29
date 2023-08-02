dadb2188ddef2f1c33d12a2648b3e06bac294c0a
TauMare <sasas.sasas.972@gmail.com>
2023-08-02 17:43:51 +0700

    Реализована генерация картинок

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import {generateUniqueID, generateImageData} from './data.js';
+import './pictures.js';
 
 const generateImageId = generateUniqueID({min: 0, max: 25});
 const generateUrlId = generateUniqueID({min: 0, max: 25});
---
+++ js/pictures.js
@@ -0,0 +1,24 @@
+import {generateUniqueID, generateImageData} from './data.js';
+
+const generateImageId = generateUniqueID({min: 0, max: 25});
+const generateUrlId = generateUniqueID({min: 0, max: 25});
+
+const photosData = [];
+for(let i = 0; i < 25; i++){
+  photosData.push(generateImageData(generateImageId(), generateUrlId()));
+}
+const photoContainer = document.querySelector('.pictures');
+const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const photoListFragment = document.createDocumentFragment();
+
+for (const photo in photosData) {
+  const photoElement = photoTemplate.cloneNode(true);
+  photoElement.querySelector('.picture__img').src = `./photos/${photosData[photo].url}`;
+  photoElement.querySelector('.picture__img').alt = photosData[photo].description;
+  photoElement.querySelector('.picture__likes').textContent = photosData[photo].likes;
+  photoElement.querySelector('.picture__comments').textContent = photosData[photo].comments.length;
+  photoListFragment.appendChild(photoElement);
+}
+
+photoContainer.appendChild(photoListFragment);
