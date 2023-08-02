92d0b6a2978fb927c8e1c2afe475281879960e8f
TauMare <sasas.sasas.972@gmail.com>
2023-08-03 01:44:46 +0700

    Промежуточный коммит, доработать под более оптимальный цикл

---
+++ js/slider.js
@@ -0,0 +1,110 @@
+const slider = document.querySelector('.effect-level__slider');
+const effectInput = document.querySelector('.effect-level__value');
+const imageNode = document.querySelector('.img-upload__preview > img');
+const activeEffects = document.querySelectorAll('.effects__radio');
+
+const effects = {
+  grayscale: {
+    filter: 'grayscale',
+    dimension: '',
+    range: {
+      min: 0,
+      max: 1,
+    },
+    start: 1,
+    step: 0.1,
+  },
+  sepia: {
+    filter: 'sepia',
+    dimension: '',
+    range: {
+      min: 0,
+      max: 1,
+    },
+    start: 1,
+    step: 0.1,
+  },
+  invert: {
+    filter: 'invert',
+    dimension: '%',
+    range: {
+      min: 0,
+      max: 100,
+    },
+    start: 100,
+    step: 1,
+  },
+  blur: {
+    filter: 'blur',
+    dimension: 'px',
+    range: {
+      min: 0,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+  },
+  brightness: {
+    filter: 'brightness',
+    dimension: '',
+    range: {
+      min: 1,
+      max: 3,
+    },
+    start: 3,
+    step: 0.1,
+  }
+};
+
+function resetParams() {
+  slider.classList.add('hidden');
+  imageNode.removeAttribute('style');
+}
+
+noUiSlider.create(slider, {
+  start: 10,
+  connect: true,
+  range: {
+    'min': 10,
+    'max': 50
+  }
+});
+
+activeEffects.forEach((effect) => {
+  effect.addEventListener('change', () => {
+    slider.classList.remove('hidden');
+    switch (effect.value) {
+      case 'chrome':
+        effect = effects.grayscale;
+        break;
+      case 'sepia':
+        effect = effects.sepia;
+        break;
+      case 'marvin':
+        effect = effects.invert;
+        break;
+      case 'phobos':
+        effect = effects.blur;
+        break;
+      case 'heat':
+        effect = effects.brightness;
+        break;
+      case 'none':
+        resetParams();
+    }
+    try {
+      slider.noUiSlider.updateOptions ({
+        range: effect.range,
+        start: effect.start,
+        step: effect.step,
+      });
+      slider.noUiSlider.on('update',() => {
+        effectInput.value = slider.noUiSlider.get();
+        imageNode.style.filter = `${effect.filter}(${effectInput.value}${effect.dimension})`;
+      });
+    } catch {
+      imageNode.removeAttribute('style');
+    }
+  });
+});
+
