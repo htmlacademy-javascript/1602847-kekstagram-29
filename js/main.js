a75c34b9e807932b71b0b1dd160da7240382b595
TauMare <sasas.sasas.972@gmail.com>
2023-08-06 03:21:23 +0700

    Теперь точно работает

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-import './pictures.js';
 import './fullPicture.js';
 import './form.js';
 import './slider.js';
 import './loadPicture.js';
 import './scale.js';
 import './fetch.js';
+
+import { fetchData } from './fetch.js';
+fetchData();
